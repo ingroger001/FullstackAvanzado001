694e218a20540e1dc730317d7ceff2e5cbb88459
ingroger001 <112985518+ingroger001@users.noreply.github.com>
2025-09-04 14:49:18 -0500

    Add files via upload

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:pro006/controladores/controlador.dart';
 import 'package:pro006/perfilpage.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -14,7 +15,7 @@ class HomePage extends StatelessWidget {
         title: Obx(
           () => Text(
             controller.nombresUsuario.value,
-            style: TextStyle(color: Colors.white),
+            style: const TextStyle(color: Colors.white),
           ),
         ),
         centerTitle: true,
@@ -29,7 +30,7 @@ class HomePage extends StatelessWidget {
               leading: const Icon(Icons.person, color: Color(0xFF5577B0)),
               title: const Text('Mi Perfil'),
               onTap: () {
-                Get.to(PerfilPage());
+                Get.to(const PerfilPage());
               },
             ),
           ),
@@ -40,7 +41,119 @@ class HomePage extends StatelessWidget {
             child: ListTile(
               leading: const Icon(Icons.qr_code, color: Color(0xFF5577B0)),
               title: const Text('Mi QR'),
-              onTap: () {},
+              onTap: () {
+                showModalBottomSheet(
+                  isScrollControlled: true,
+                  context: context,
+                  builder: (x) {
+                    return Scaffold(
+                      appBar: AppBar(
+                        title: Text(
+                          'Carnet ',
+                          style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                      body: Padding(
+                        padding: const EdgeInsets.all(20.0),
+                        child: Obx(() {
+                          final userData = controller.datosUsuario.value;
+                          final imageUrl = userData['urlfoto'] ?? '';
+                          final email = userData['email'] ?? '';
+
+                          return Card(
+                            elevation: 8,
+
+                            child: Padding(
+                              padding: const EdgeInsets.all(16.0),
+                              child: ListView(
+                                children: [
+                                  CircleAvatar(
+                                    backgroundColor: Color(0xFF5577B0),
+                                    radius: 65,
+                                    child: CircleAvatar(
+                                      radius: 60,
+                                      backgroundColor: Colors.grey[300],
+                                      child:
+                                          imageUrl.isEmpty
+                                              ? const Icon(
+                                                Icons.person,
+                                                size: 60,
+                                                color: Colors.grey,
+                                              )
+                                              : ClipOval(
+                                                child: Image.network(
+                                                  imageUrl,
+                                                  width: 120,
+                                                  height: 120,
+                                                  fit: BoxFit.cover,
+                                                  errorBuilder: (
+                                                    context,
+                                                    error,
+                                                    stackTrace,
+                                                  ) {
+                                                    return const Icon(
+                                                      Icons.person,
+                                                      size: 60,
+                                                      color: Colors.grey,
+                                                    );
+                                                  },
+                                                ),
+                                              ),
+                                    ),
+                                  ),
+                                  Divider(),
+                                  if (email.isNotEmpty)
+                                    Center(
+                                      child: QrImageView(
+                                        data: email,
+                                        version: QrVersions.auto,
+                                        size: 220.0,
+                                        backgroundColor: Colors.white,
+                                      ),
+                                    ),
+                                  Divider(),
+
+                                  Container(
+                                    padding: EdgeInsets.all(10),
+                                    color: Color(0xFF5577B0),
+                                    child: Center(
+                                      child: Text(
+                                        userData['nombres'] ?? 'N/A',
+                                        style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 18,
+                                          color: Colors.white,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  Center(
+                                    child: Text(
+                                      userData['email'] ?? 'N/A',
+                                      style: TextStyle(
+                                        fontStyle: FontStyle.italic,
+                                      ),
+                                    ),
+                                  ),
+                                  Center(
+                                    child: Text(userData['telefono'] ?? 'N/A'),
+                                  ),
+
+                                  const SizedBox(height: 20),
+                                ],
+                              ),
+                            ),
+                          );
+                        }),
+                      ),
+                    );
+                  },
+                );
+              },
             ),
           ),
           Obx(
